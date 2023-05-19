0da3cb1fbc2025c0c142e61c9fe9e948babad05c
HomeIT Admin <homeitadmin@seypro.de>
2023-05-19 10:07:46 +0000

    Next step

--- gitpod/custom-entrypoint.sh
+++ gitpod/custom-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh 
   
- # Both gitpod and Nextcloud need read/write acess 
- # Obviously not recommended for a production system 
- chmod -R 777 /var/www/html/custom_apps/ 
+ chown www-data:www-data /var/www/html/custom_apps/ 
   
  /entrypoint.sh "$@"
