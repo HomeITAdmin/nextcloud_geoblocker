19f1ee31ef0b3e21a1f7881422909574ff9e3de2
HomeIT Admin <homeitadmin@seypro.de>
2023-05-21 10:49:25 +0000

    Add more comfort

--- gitpod/custom-entrypoint.sh
+++ gitpod/custom-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh 
   
- chown www-data:www-data /var/www/html/custom_apps/ 
+chown www-data:www-data /var/www/html/custom_apps/ 
+
+sudo -u#33 php occ app:enable geoblocker  
   
- /entrypoint.sh "$@"
+/entrypoint.sh "$@"
