b94ad91c244e90053446000e7eb8e5e733cfdcf6
HomeITAdmin <homeitadmin@seypro.de>
2025-09-28 10:51:10 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
@@ -15,3 +15,15 @@ require_once __DIR__.'/../../../lib/base.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 OC_Hook::clear();
+*/
+
+declare(strict_types=1);
+
+use OCP\App\IAppManager;
+use OCP\Server;
+
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('geoblocker');
