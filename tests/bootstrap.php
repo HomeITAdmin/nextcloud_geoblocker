7e3b633ce6042f4e78d08f830fc1ad5b30d18662
HomeITAdmin <homeitadmin@seypro.de>
2025-09-28 11:02:19 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,4 @@
 <?php
-/*
-if (!defined('PHPUNIT_RUN')) {
-	define('PHPUNIT_RUN', 1);
-}
-
-require_once __DIR__.'/../../../lib/base.php';
-
-// Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-
-// Fix for "Autoload path not allowed: .../test/tests/testcase.php"
-\OC_App::loadApp('geoblocker');
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-OC_Hook::clear();
-*/
 
 declare(strict_types=1);
 
